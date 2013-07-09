5714453dcbc9bb52177f08087d5b0a74bbdd5f49
woying <loveson821@gmail.com>
2013-07-10 01:01:55 +0800

    7.5.6.3 image photo size gen

--- fake/resize.js
+++ fake/resize.js
@@ -5,47 +5,14 @@ var fs = require('fs');
 
 dir = path.resolve('../public/images/uploads/');
 var imgs = fs.readdirSync(dir);
-// var params = [];
-
-
-//console.log(params);
-
-
-// async.mapSeries(params, ei.thumbnails, function(){
-//     console.log('finish');
-// });
-
-
-
-
 
 // add some items to the queue
+var srcPath = []
 for( img in imgs){
-    srcPath = path.resolve('../public/images/uploads/'+imgs[img]);
-    // q.push({srcPath: srcPath}, function(err){
-    // 	console.log('finished processing foo');
-    // });
-    ei.thumbnails(srcPath, 'undefined', function(image){
-        console.log(image);
-    });
+    srcPath.push(path.resolve('../public/images/uploads/'+imgs[img]))
 }
 
+ei.thumbnails(srcPath, 'undefined', function(image){
+    console.log(image);
+});
 
-// q.push({name: 'foo'}, function (err) {
-//     console.log('finished processing foo');
-// });
-// q.push({name: 'bar'}, function (err) {
-//     console.log('finished processing bar');
-// });
-
-// add some items to the queue (batch-wise)
-
-// q.push([{name: 'baz'},{name: 'bay'},{name: 'bax'}], function (err) {
-//     console.log('finished processing bar');
-// });
-
-// add some items to the front of the queue
-
-// q.unshift({name: 'bar'}, function (err) {
-//     console.log('finished processing bar');
-// });
